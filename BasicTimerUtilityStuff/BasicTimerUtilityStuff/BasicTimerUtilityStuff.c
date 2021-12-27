78de2cb83331d73b22daedadae1ea8dbd3aa5da9
Korporal <hugh_gleaves@hotmail.com>
2021-12-27 11:07:28 -0700

    Flesh out a reasonable first cut that does this:
    https://forum.allaboutcircuits.com/threads/flashing-three-leds.183918/#post-1693805

--- BasicTimerUtilityStuff/BasicTimerUtilityStuff/BasicTimerUtilityStuff.c
+++ BasicTimerUtilityStuff/BasicTimerUtilityStuff/BasicTimerUtilityStuff.c
@@ -1,6 +1,9 @@
 // BasicTimerUtilityStuff.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#define ON 1
+#define OFF 0
+
 typedef enum state_st
 {
     STOPPED,
@@ -15,44 +18,77 @@ typedef struct task_st
     State state;        // state of task
     int duration;       // timer duration
     int value;          // current counted value
-    int payload;        // the LED ID 
+    int led;            // the LED ID 
+    int next_state;     // on or off
     ActionFunc action;  // what to do when we expire
 
 } Task, * Task_ptr;
 
-void handler(Task_ptr);
+void expiration_handler(int);
 void interrupt();
-void init_task(Task_ptr,int,int,ActionFunc);
+void initialize_task(Task_ptr, State, int, int, int, ActionFunc);
+void set_led(int, int);
 
 static Task tasks[6];   // six task instances, not yet initialized
 
 int main()
 {
+    // Initialize all the tasks, none are 'running' just yet
 
-    tasks[0].state = STOPPED;
+    initialize_task(&tasks[0], STOPPED, 100, 1, OFF, expiration_handler); 
+    initialize_task(&tasks[1], STOPPED, 200, 2, OFF, expiration_handler); 
+    initialize_task(&tasks[2], STOPPED, 300, 3, OFF, expiration_handler);  
 
+    set_led(1, ON);
+    set_led(2, ON);
+    set_led(3, ON);
 
+    tasks[0].state = RUNNING;
+    tasks[1].state = RUNNING;
+    tasks[2].state = RUNNING;
+
+    // do whatever else is needed, eg. get system 10 mS timer running etc.
 }
 
-void handler(Task_ptr task)
+void expiration_handler(int task_id)
 {
-
-    return;
+    set_led(tasks[task_id].led, tasks[task_id].next_state);
+    tasks[task_id].next_state = !tasks[task_id].next_state;
+    tasks[task_id].state = RUNNING; // restar this task
 }
 
-void init_task(Task_ptr task_ptr, int duration, int payload, ActionFunc action)
+void initialize_task(Task_ptr task_ptr, State initial, int duration, int led, int led_status, ActionFunc action)
 {
-    task_ptr->state = STOPPED;
+    task_ptr->state = initial;
     task_ptr->duration = duration;
     task_ptr->value = 0;
-    task_ptr->payload = payload;
+    task_ptr->led = led;
+    task_ptr->next_state = led_status;
     task_ptr->action = action;
-
 }
 
-// This is called by the system, timer interrupt etc
+// This is called by the system, timer interrupt etc, assume every 10 mS
 void interrupt()
 {
 
+    for (int I = 0; I < 6; I++)
+    {
+        if (tasks[I].state == RUNNING)
+        {
+            tasks[I].value += 10;
 
+            if (tasks[I].value >= tasks[I].duration)
+            {
+                tasks[I].value = 0;
+                tasks[I].action(I); // this should be a fast executing bit of code, nothing costly...
+                tasks[I].state = STOPPED;
+            }
+        }
+    }
+
+}
+
+void set_led(int LED, int state)
+{
+    // do whatever to set LED on or off
 }
